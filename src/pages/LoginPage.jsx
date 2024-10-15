77efd7247a0e08a9f05a307dd9febf0b251366fb
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-10-16 09:47:36 +1300

    feat(component): updated routing
    fixed navigation redirects to un-used pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,14 @@
-import LoginForm from '../components/forms/LoginForm';
-import { useNavigate } from 'react-router-dom';
-import { useUserAuth } from '../context/FirestoreAuthContext';
+import LoginForm from "../components/forms/LoginForm";
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../context/FirestoreAuthContext";
+import { routerPaths } from "../router/routeLabels";
 
 const LoginPage = () => {
   const { user } = useUserAuth();
   const navigate = useNavigate();
 
   if (user) {
-    navigate('/dashboard');
+    navigate(routerPaths.eventPlanner);
   }
 
   return <LoginForm />;
